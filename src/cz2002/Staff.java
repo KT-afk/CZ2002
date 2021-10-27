38687ca8334e18f387f26c73fbb4b77366d10f3f
CharlesTran267 <75065355+CharlesTran267@users.noreply.github.com>
2021-10-28 00:41:12 +0800

    Update Staff.java

--- src/cz2002/Staff.java
+++ src/cz2002/Staff.java
@@ -4,7 +4,7 @@ public class Staff extends Person{
 	public static Integer StaffIDcounter=0;
 	private Integer id;
 	private String jobTitle;
-	public Customer(String name, Gender gender, String jobTitle) {
+	public Staff(String name, Gender gender, String jobTitle) {
 		super(name,gender);
 		id = StaffIDcounter++;
 		this.jobTitle = jobTitle;
