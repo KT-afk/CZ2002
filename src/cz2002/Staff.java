b5d3a824d1c11031e95cf1f6b4e31bcfc770a0f0
CharlesTran267 <75065355+CharlesTran267@users.noreply.github.com>
2021-10-27 20:21:36 +0800

    Update Staff.java

--- src/cz2002/Staff.java
+++ src/cz2002/Staff.java
@@ -1,7 +1,7 @@
 import Person.Gender;
 
 public class Customer extends Person{
-	public static int StaffIDcounter=0;
+	public static Integer StaffIDcounter=0;
 	private Integer id;
 	private String jobTitle;
 	public Customer(String name, Gender gender, String jobTitle) {
