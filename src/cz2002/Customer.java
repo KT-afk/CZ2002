98e75c09ef6ee2dc6638e7cab3c1a6189d9ba5fd
CharlesTran267 <75065355+CharlesTran267@users.noreply.github.com>
2021-10-28 00:43:53 +0800

    Update Customer.java

--- src/cz2002/Customer.java
+++ src/cz2002/Customer.java
@@ -4,9 +4,11 @@ import Person.Gender;
 public class Customer extends Person{
 	public static Integer CustIDcounter=0;
 	private Integer id;
-	public Customer(String name, Gender gender) {
+	private boolean membership;
+	public Customer(String name, Gender gender, boolean membership) {
 		super(name,gender);
 		id = CustIDcounter++;
+		this.membership = membership;
 	}
 	
 }
