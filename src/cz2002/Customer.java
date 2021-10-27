b7c565dcfe0c893cb76ced398b8b5c78ec0450e5
CharlesTran267 <75065355+CharlesTran267@users.noreply.github.com>
2021-10-27 20:20:21 +0800

    Update Customer.java

--- src/cz2002/Customer.java
+++ src/cz2002/Customer.java
@@ -2,7 +2,7 @@
 import Person.Gender;
 
 public class Customer extends Person{
-	public static int CustIDcounter=0;
+	public static Integer CustIDcounter=0;
 	private Integer id;
 	public Customer(String name, Gender gender) {
 		super(name,gender);
