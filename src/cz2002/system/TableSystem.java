eb9f16880b564cd954a72b0370db17b07857f224
Siddiq <devsfiq@gmail.com>
2021-11-03 21:32:27 +0800

    Added TableSystem

---
+++ src/cz2002/system/TableSystem.java
@@ -0,0 +1,13 @@
+package cz2002.system;
+
+import cz2002.entity.Table;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TableSystem {
+
+    public List<Table> getTables() {
+        return new ArrayList<>();
+    }
+}
