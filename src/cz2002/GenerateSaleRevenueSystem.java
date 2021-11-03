5aace1bb1dc6ca8c00d87517b3466e5847e67320
CharlesTran267 <75065355+CharlesTran267@users.noreply.github.com>
2021-11-03 19:02:54 +0800

    Update GenerateSaleRevenueSystem.java

--- src/cz2002/GenerateSaleRevenueSystem.java
+++ src/cz2002/GenerateSaleRevenueSystem.java
@@ -1,7 +1,17 @@
 package cz2002;
 
-public class GenerateSaleRevenueSystem {
-	public void generateSaleRevenueRep () {
-		
+import java.util.*;
+
+public class GenerateSaleRevenueSystem{
+	public void generateSaleRevenueRep(Date start, Date end) {
+		ArrayList<Order> orderListIncluded;
+		double totalPrice=0;
+		//suppose OrderList is where we store all orders
+		for(int i=0;i<OrderList.size();i++) {
+			if(!start.after(OrderList.get(i).start) && !end.before(OrderList.get(i).end)) {
+				orderListIncluded.add(OrderList.get(i));
+				totalPrice+=OrderList.get(i).totalPrice();
+			}
+		}
 	}
 }
