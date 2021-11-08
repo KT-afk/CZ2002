68bf1490248fc1731d3a66dd7d11ee98a925177a
Siddiq <devsfiq@gmail.com>
2021-11-08 21:39:50 +0800

    Added JavaDoc

--- src/cz2002/ui/SaleRevenueUI.java
+++ src/cz2002/ui/SaleRevenueUI.java
@@ -5,22 +5,41 @@ import cz2002.entity.Order;
 import cz2002.entity.SaleRevenue;
 import cz2002.system.SaleRevenueSystem;
 
-import java.awt.*;
 import java.text.SimpleDateFormat;
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
 import java.util.Date;
 import java.util.Scanner;
 
+/**
+ *     User Interface for SaleRevenueSystem
+ *     @author Abdul Siddiq
+ *     @version 1.0
+ *     @since 2020-11-08
+ */
+
 public class SaleRevenueUI {
+    /**
+        Scanner
+     */
     private Scanner sc;
+
+    /**
+         Sale Revenue System
+     */
     private SaleRevenueSystem saleRevenueSystem;
 
+    /**
+     * Creates a SaleRevenueUI that interfaces with SaleRevenueSystem to perform its functionality.
+     * @param saleRevenueSystem Sale Revenue System
+     * @param scanner Scanner object.
+     */
     public SaleRevenueUI(SaleRevenueSystem saleRevenueSystem, Scanner scanner) {
         this.saleRevenueSystem = saleRevenueSystem;
         sc = scanner;
     }
 
+    /**
+     * Runs 'Print Sale Revenue' routine
+     */
     public void printSaleRevenueReport() {
         Date startDate = promptDate("Please enter start period");
         Date endDate = promptDate("Please enter end period");
@@ -33,6 +52,10 @@ public class SaleRevenueUI {
         printSaleRevenue(saleRevenueSystem.generateSaleRevenueRep(startDate, endDate));
     }
 
+    /**
+     * Prints Sale Revenue
+     * @param saleRevenue SaleRevenue object to print
+     */
     public void printSaleRevenue(SaleRevenue saleRevenue) {
         var orderList = saleRevenue.getOrderList();
 
@@ -43,6 +66,10 @@ public class SaleRevenueUI {
         System.out.printf("\nTotal Revenue: %.2f", saleRevenue.getTotalPrice());
     }
 
+    /**
+     * Prints Order
+     * @param order Order object to print
+     */
     private void printOrder(Order order) {
         System.out.printf("[%s] Order #%-2d - $%.2f\n", order.getStart(), order.getID(), order.totalPrice());
         for(MenuItem menuItem : order.getPackItems())
@@ -52,10 +79,18 @@ public class SaleRevenueUI {
             printMenuItem(menuItem);
     }
 
+    /**
+     * Prints Menu Item
+     * @param menuItem MenuItem object to print
+     */
     private void printMenuItem(MenuItem menuItem) {
         System.out.printf(" - %-10s\n", menuItem.getName());
     }
 
+    /**
+     * Request user to key in Date
+     * @param prompt Prompt to print out when requesting
+     */
     private Date promptDate(String prompt) {
         SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
 
