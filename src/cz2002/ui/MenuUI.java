5e7022461e0abe4c877087e4b6eae0662d54e69e
BenCheong98 <benjamincheong98@gmail.com>
2021-11-09 01:05:51 +0800

    Menu Javadoc

--- src/cz2002/ui/MenuUI.java
+++ src/cz2002/ui/MenuUI.java
@@ -1,6 +1,5 @@
 package cz2002.ui;
 
-import cz2002.entity.FoodDish;
 import cz2002.entity.MenuItem;
 import cz2002.util.ScannerUtil;
 
@@ -9,27 +8,67 @@ import java.util.List;
 import java.util.Scanner;
 import java.util.stream.Stream;
 
+/**
+ * User Interface for Managing Menu Items
+ * 
+ * @author Benjamin Cheong
+ * @version 1.0
+ * @since 2021-11-08
+ *
+ */
 public abstract class MenuUI {
-
+	/**
+	 * Scanner
+	 */
     protected Scanner sc;
+    /**
+     * List of Menu Items
+     */
     protected List<MenuItem> menu;
-
+    
+    /**
+     * String of the formatting for headers
+     */
     private String format = "";
+    /**
+     * String array of headers to be printed
+     */
     private String[] headers = {};
-
+    
+    /**
+     * Creates new User Interface for managing Menu Items
+     * @param scanner Scanner object
+     * @param restaurantMenu List of Menu Items
+     */
     public MenuUI(Scanner scanner, List<? extends MenuItem> restaurantMenu) {
         sc = scanner;
         menu = (List<MenuItem>) restaurantMenu;
     }
-
+    
+    /**
+     * Sets format and headers attribute
+     * @param format New format
+     * @param headers New headers
+     */
     public void setFormatting(String format, String... headers) {
         this.format = format;
         this.headers = headers;
     }
-
+    
+    /**
+     * User Interface for Creating Menu Item
+     */
     abstract void createMenuItem();
+    /**
+     * User Interface for Editing Menu Item
+     * @param item Menu Item to be edited
+     */
     abstract void editMenuItem(MenuItem item);
-
+    /**
+     * User Interface for Managing Menu Item
+     * Invokes corresponding User Interfaces upon selection
+     * @param title Title that indicates whether managing individual food dish or set package
+     */
     public void run(String title) {
 
         boolean running = true;
@@ -46,7 +85,11 @@ public abstract class MenuUI {
             }
         }
     }
-
+    
+    /**
+     * User Interface for selecting which menu item to edit
+     * Invokes user interface for editing selected menu item
+     */
     protected void selectEditMenuItem() {
         while (true) {
             String[] menuItems = menu.stream().map(item -> item.toString()).toArray(String[]::new);
@@ -62,7 +105,10 @@ public abstract class MenuUI {
             }
         }
     }
-
+    /**
+     * User Interface for selecting which menu item to remove
+     * Removes menu item upon selection
+     */
     protected void selectRemoveMenuItem() {
         while (true) {
             String[] menuItems = menu.stream().map(item -> item.toString()).toArray(String[]::new);
@@ -79,7 +125,10 @@ public abstract class MenuUI {
             }
         }
     }
-
+    
+    /**
+     * Prints all menu items in menu
+     */
     protected void printMenu() {
         System.out.println("\nContents of Restaurant Menu Dishes: ");
         System.out.printf(format + "\n", headers);
